39efd0711274882e36bc1281ac8bc633ae765e73
schen888 <shuang.chen888@hotmail.com>
2022-09-19 18:44:35 +0200

    Add cross out function for list item.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 function newItem(){
     let inputValue=$('#input').val();
     let list=$('#list');
+    let li=$('<li></li>');
+    list.append(li);
 
     if (inputValue==='') {
         alert('You must write something.');
     } else {
-        let li=$('<li></li>');
         li.append(inputValue);
-        list.append(li);
         let crossOutButton=$('<crossOutButton></crossOutButton>');
         crossOutButton.append(document.createTextNode('X'));
         li.append(crossOutButton);
     }
+
+/*     function crossOut(){
+        li.toggleClass('strike');
+    }
+     */
+    li.dblclick(()=>li.toggleClass('strike'));
+
+
 }
